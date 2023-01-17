3ca5f89a0c9bbadf4cd5fc0c9734ccf40d823314
Kevin Diesenberg <kdiesenb@gmail.com>
2023-01-17 15:44:42 -0500

    finished up

--- app/controllers/leases_controller.rb
+++ app/controllers/leases_controller.rb
@@ -1,2 +1,33 @@
 class LeasesController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: not_found_message
+rescue_from ActiveRecord::RecordInvalid, with: invalid_record
+
+    def create
+        lease = Lease.create!(lease_params)
+        render json: lease, status: :created
+    end
+
+    def delete
+        lease = find_lease
+        lease.destroy
+        head :no_conten
+    end
+
+    private
+
+    def invalid_record(invalid)
+        render json: {error: invalid.record.errors.full_messages }, status: :unprocessable_entity
+    end
+
+    def lease_params
+        params.permit(:rent)
+    end
+
+    def find_lease
+        lease = Lease.find(params[:id])
+    end
+
+    def not_found_message
+        render json: {error: "Lease not found"}
+    end
 end
--- app/controllers/tenants_controller.rb
+++ app/controllers/tenants_controller.rb
@@ -1,2 +1,48 @@
 class TenantsController < ApplicationController
+    rescue_from ActiveRecord::RecordInvalid, with: invalid_record
+    rescue_from ActiveRecord::RecordNotFound, with: record_not_found
+
+    def index
+        tenant = Tenant.all
+        render json: tenant
+    end
+
+    def show
+        tenant = find_tenant
+        render json: tenant
+    end
+
+    def create
+        tenant = Tenant.create!(tenant_params)
+        render json: tenant
+    end
+
+    def update
+        tenant = Tenant.update!(tenant_params)
+        render json: tenant
+    end
+
+    def destroy
+        tenant = find_tenant
+        tenant.destroy
+        head :no_content
+    end
+
+    private
+
+    def tenant_params
+        params.permit(:name, :age)
+    end
+
+    def find_tenant
+        find_tenant = Tenant.find(params[:id])
+    end
+
+    def invalid_record(invalid)
+        render json: { error: invalid.record.errors.full_messages }, status: :unprocessable_entity
+    end
+
+    def not_found
+        render json: { error: 'Not Found' }, status: :not_found
+    end
 end
