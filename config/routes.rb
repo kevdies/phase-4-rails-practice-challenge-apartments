64dc650e76021421267c469e37076655c97f5a96
Kevin Diesenberg <kdiesenb@gmail.com>
2023-01-17 14:25:24 -0500

    models and associations migrated and seeded

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :leases, only: [:create, :destroy]
+  resources :tenants, only: [:index, :show, :update, :destory]
+  resources :apartments, only: [:index, :show, :create, :update, :destroy]
 end
